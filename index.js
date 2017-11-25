e7ffe7685b19d3ee4ae084cc2cbd0660743cec7b
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2017-11-25 18:59:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,7 @@ const createFareMultiplier = function (value) {
 
 const fareDoubler = createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(drivers, selectDrivers){
+  return selectDrivers(drivers);
+}
